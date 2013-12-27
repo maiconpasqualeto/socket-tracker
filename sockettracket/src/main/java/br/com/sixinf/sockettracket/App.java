6f30b5aa0f0f17924bd96cc0bed7ef404bbaa12d
Maicon Dante <maiconpas@yahoo.com.br>
2013-12-27 19:42:43 -0300

    primeiro commit

---
+++ sockettracket/src/main/java/br/com/sixinf/sockettracket/App.java
@@ -0,0 +1,13 @@
+package br.com.sixinf.sockettracket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
